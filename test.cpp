1abb07a36e8a3d3036e7320a79fc415a1bfa288f
jiangyili446 <70182589+jiangyili446@users.noreply.github.com>
2021-10-17 20:21:54 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,11 +4,13 @@
 #include<string.h>
 int main(int argc, char *argv[])
 {
-	FILE *fp;
+	FILE *fp,*fp2;
 	fp=fopen(argv[1],"r");
+	fp2=fopen(argv[2],"w");
 	char c;
 	int a=0;
 	int b=0;
 	printf("define  dso_local i32 @main(){ret i32 100}");
+	fprintf(fp2,"define  dso_local i32 @main(){ret i32 100}");
 	return 0;
 }
