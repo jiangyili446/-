5160746973a9e567f59336b7d09e6ed893ac8305
jiangyili446 <70182589+jiangyili446@users.noreply.github.com>
2021-11-10 18:04:18 +0800

    Update and rename lab2.java to lab3.java

---
+++ lab3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Locale;
+
+public class lab3 {
+
+    
+    public static void main(String[] args) throws IOException {
+
+        File inputfile = new File(args[0]);
+        
+        FileReader a=new FileReader(inputfile);
+        for(int i=0;i<inputfile.length();i++){
+            System.out.print(a.read());
+        }
+
+    }
+
+    
